71d7b3643366bdf174d6bfcc109ac6dd9f5ba4da
dfeli014 <dfeli014@fiu.edu>
2019-01-15 13:04:12 -0500

    Completed

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,16 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
-
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else if (n > 1) {
+        return fib(n-1)+fib(n-2); 
+    }
 }
+
+// Use cases for the Fibonacci Function 
+console.log(fib(3));
+console.log(fib(6));
+console.log(fib(9));
